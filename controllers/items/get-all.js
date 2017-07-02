e0102dec3ea070c2f9afb62a98160395f8dd0594
Hemerson Vianna <hemerson.lourenco@gmail.com>
2017-07-02 14:08:55 -0300

    feature(get all): add filters of status and scope

--- controllers/items/get-all.js
+++ controllers/items/get-all.js
@@ -2,8 +2,19 @@ const mongodb = require('../../lib/mongodb');
 
 module.exports = function(req, res, next) {
   const collection = mongodb.get().collection('items');
+  const query = {};
+  const status = req.query.status;
+  const scope = req.query.scope;
 
-  collection.find().toArray(function(err, docs) {
+  if(status) {
+    query.status = status;
+  }
+
+  if(scope) {
+    query['criteria.scope.include'] = { '$in': [ scope ] };
+  }
+
+  collection.find(query).toArray(function(err, docs) {
     res.status(200).json({items: docs});
   })
 };
